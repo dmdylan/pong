1a696d3e12538039c7dcdbb0175520528e3eaebc
dmdylan <51095343+dmdylan@users.noreply.github.com>
2019-05-31 22:11:07 -0400

    Add files via upload
    The c# scripts used

---
+++ Scripts/SoundController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public AudioSource wallSound;
+    public AudioSource racketSound;
+    public AudioSource pointSound;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name=="Player1Racket" || collision.gameObject.name == "Player2Racket")
+        {
+            this.racketSound.Play();
+        }
+        else if(collision.gameObject.name=="TopSide" || collision.gameObject.name == "BottomSide")
+        {
+            this.wallSound.Play();
+        }
+        else
+        {
+            this.pointSound.Play();
+        }
+    }
+}
